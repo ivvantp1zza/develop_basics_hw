c03f1a0c91c181658a91c4aefcc56a784b46102f
Rafael Ishmuhametov <rafael.ishmuhametov2004@gmail.ru>
2022-10-02 17:27:02 +0300

    week4 HW

---
+++ week4/HttpServer/HttpServer/HttpServer.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+
+namespace NetConsoleApp;
+
+public class HttpServer
+{
+    private static HttpListener _listener;
+    private bool ServerIsRunning = false;
+    public HttpServer(string url)
+    {
+        _listener = new HttpListener();
+        _listener.Prefixes.Add(url);
+    }
+
+    public void Start()
+    {
+        if (ServerIsRunning)
+            return;
+        _listener.Start();
+        ServerIsRunning = true;
+        Manage();
+        Console.WriteLine("Server started");
+    }
+
+    public async Task Manage()
+    {
+        while (true)
+        {
+                var context = await _listener.GetContextAsync();
+                var request = context.Request;
+                var response = context.Response;
+                byte[] googleBuffer;
+                await using (var sourceStream = 
+                             File.Open(@"C:\Games\IL_Prac\HttpServer\HttpServer\index.html", FileMode.Open))
+                {
+                    googleBuffer = new byte[sourceStream.Length];
+                    sourceStream.Read(googleBuffer, 0, (int)sourceStream.Length);
+                }
+                response.ContentLength64 = googleBuffer.Length;
+                var output = response.OutputStream;
+                output.Write(googleBuffer, 0, googleBuffer.Length);
+                output.Close();
+        }
+    }
+
+    public void Stop()
+    {
+        if (!ServerIsRunning)
+            return;
+        _listener.Stop();
+        ServerIsRunning = false;
+        Console.WriteLine("Server stopped");
+        
+    }
+}
---
+++ week4/HttpServer/HttpServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.IO;
+ 
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer("http://localhost:1488/google/");
+            server.Start();
+            while(true)
+            {
+                ReadCommand(Console.ReadLine(), server);
+            }
+        }
+        static void ReadCommand(string command, HttpServer server)
+        {
+            switch (command)
+            {
+                case "start": 
+                    server.Start();
+                    break;
+                case "stop":
+                    server.Stop();
+                    break;
+                case "restart":
+                    server.Stop();
+                    server.Start();
+                    break;
+                default:
+                    Console.WriteLine("Unknown command!");
+                    break;
+            }
+        }
+    }
+}
